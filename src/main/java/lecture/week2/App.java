550270dc4c09a3524fbb404da4fff1a71a7ce0d8
khyrdantai <harry_hocker@icloud.com>
2021-06-03 22:46:03 -0400

    Initial commit.

---
+++ src/main/java/lecture/week2/App.java
@@ -0,0 +1,15 @@
+/*
+ * UCF COP3330 Summer 2021
+ * Copyright 2021 Harry Hocker
+ */
+
+package lecture.week2;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("helloworld!");
+
+    }
+}
