8d4d5a586991af20b009a3033041586aa61b7ea3
Monz <69062179+khyrdantai@users.noreply.github.com>
2021-06-07 20:19:17 -0400

    Exercise 31 completed.

--- src/main/java/exercises/assignment2/ex31/EX31.java
+++ src/main/java/exercises/assignment2/ex31/EX31.java
@@ -3,34 +3,6 @@
  * Copyright 2021 Harry Hocker
  */
 
-/*
-Exercise 31 - Karvonen Heart Rate
-When you loop, you can control how much you increment the counter; you don’t always have to increment by one.
-
-When getting into a fitness program, you may want to figure out your target heart rate so you don’t overexert yourself.
-The Karvonen heart rate formula is one method you can use to determine your rate.
-Create a program that prompts for your age and your resting heart rate.
-Use the Karvonen formula to determine the target heart rate based on a range of intensities from 55% to 95%.
-Generate a table with the results as shown in the example output. The formula is
-
-TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
-Example Output
-Resting Pulse: 65        Age: 22
-
-Intensity    | Rate
--------------|--------
-55%          | 138 bpm
-60%          | 145 bpm
-65%          | 151 bpm
-:               :        (extra lines omitted)
-85%          | 178 bpm
-90%          | 185 bpm
-95%          | 191 bpm
-Constraints
-Don’t hard-code the percentages. Use a loop to increment the percentages from 55 to 95.
-Ensure that the heart rate and age are entered as numbers. Don’t allow the user to continue without entering valid inputs.
-Display the results in a tabular format.
- */
 
 package exercises.assignment2.ex31;
 
@@ -53,7 +25,7 @@ public class EX31 {
 	public void runProgram() {
 
 		String str1 = "Enter your resting heart-rate (BPM): ";
-		String str2 = "Enter your age: \n";
+		String str2 = "Enter your age: ";
 
 		int heartRate = Integer.parseInt(getInput(str1));
 		int age = Integer.parseInt(getInput(str2));
@@ -63,7 +35,9 @@ public class EX31 {
 
 	private void printCalculations(int heartRate, int age) {
 
-		System.out.printf("Resting Pulse: %d \t \t Age: %d \n", heartRate, age);
+		System.out.printf("Resting Pulse: %d \t \t Age: %d \n\n", heartRate, age);
+		System.out.print("Intensity \t | Rate\n");
+		System.out.println("-------------|--------");
 
 		for (int i = 55; i <= 95 ; i+=5) {
 
